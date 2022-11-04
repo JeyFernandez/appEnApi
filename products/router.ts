ccb6fb2a527657304f2d3e02517f3e9ab69e1e97
unknown <jeicobfer@gmail.com>
2022-11-04 14:49:38 -0600

    agregando dos crud en el api

---
+++ products/router.ts
@@ -0,0 +1,44 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { requireAuth } from '../auth/middlewares';
+import controller from './controller';
+
+const router = Router();
+
+
+router.get("/", requireAuth, async (req: Request, res: Response, next: NextFunction) => {
+    const productos = await controller.list();
+    res.json(productos);
+});
+
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const product = await controller.store(req.body)
+        res.status(201).json(product);
+    } catch (error) {
+        res.json({
+            message: error
+        });
+    }
+});
+//https://www.tutofox.com/react-native/react-native-como-conectar-y-listar-datos-desde-api-rest/
+
+router.get("/:id", requireAuth, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const product = await controller.getOne(id);
+        res.json(product);
+    } catch (error: any) {
+        res.json({
+            message: error.message
+        });
+    }
+});
+
+router.delete("/:id", (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    // let myproducts = products.filter(item => item.id !== id);
+    res.json({});
+});
+
+export default router; 
